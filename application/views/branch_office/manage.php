636e85cbba8781c1ab09899d780b066e74cd7c10
JhonatanPacheco <jhonatanpachecohernandez@gmail.com>
2018-05-10 11:58:03 -0500

    Update manage.php

--- application/views/branch_office/manage.php
+++ application/views/branch_office/manage.php
@@ -3,57 +3,34 @@
 <script type="text/javascript">
 $(document).ready(function()
 {
-    $('#generate_barcodes').click(function()
-    {
-        window.open(
-            'index.php/items/generate_barcodes/'+table_support.selected_ids().join(':'),
-            '_blank' // <- This is what makes it open in a new window.
-        );
-    });
-	
-	
-
-	// load the preset datarange picker
-	<?php $this->load->view('partial/daterangepicker'); ?>
-    // set the beginning of time as starting date
-//    $('#daterangepicker').data('daterangepicker').setStartDate("<?php echo date($this->config->item('dateformat'), mktime(0,0,0,01,01,2010));?>");
-	// update the hidden inputs with the selected dates before submitting the search data
-    var start_date = "<?php echo date('Y-m-d', mktime(0,0,0,01,01,2010));?>";
-	
-
-    $("#stock_location").change(function() {
-       table_support.refresh();
-    });
-
-    <?php $this->load->view('partial/bootstrap_tables_locale'); ?>
+	<?php $this->load->view('partial/bootstrap_tables_locale'); ?>
 
     table_support.init({
-        employee_id: <?php echo $this->Employee->get_logged_in_employee_info()->person_id; ?>,
+        //employee_id: <?php echo $this->Employee->get_logged_in_employee_info()->person_id; ?>,
         resource: '<?php echo site_url($controller_name);?>',
         headers: <?php echo $table_headers; ?>,
         pageSize: <?php echo $this->config->item('lines_per_page'); ?>,
         uniqueId: 'branch_office.branch_office_id',
-        queryParams: function() {
-            return $.extend(arguments[0], {
-                start_date: start_date,
-                end_date: end_date,
-                stock_location: $("#stock_location").val(),
-                filters: $("#filters").val() || [""]
-            });
-        },
-        onLoadSuccess: function(response) {
-            $('a.rollover').imgPreview({
-				imgCSS: { width: 200 },
-				distanceFromCursor: { top:10, left:-210 }
-			})
-        }
+        enableActions: function()
+		{
+			var email_disabled = $("td input:checkbox:checked").parents("tr").find("td a[href^='mailto:']").length == 0;
+			$("#email").prop('disabled', email_disabled);
+		}
     });
+	
+	$("#email").click(function(event)
+	{
+		var recipients = $.map($("tr.selected a[href^='mailto:']"), function(element)
+		{
+			return $(element).attr('href').replace(/^mailto:/, '');
+		});
+		location.href = "mailto:" + recipients.join(",");
 });
 </script>
 
 <div id="title_bar" class="btn-toolbar print_hide">
     <button class='btn btn-info btn-sm pull-right modal-dlg' data-btn-submit='<?php echo $this->lang->line('common_submit') ?>' data-href='<?php echo site_url($controller_name."/excel_import"); ?>'
-            title='<?php echo $this->lang->line('items_import_items_excel'); ?>'>
+            title='<?php echo $this->lang->line('branch_office_import_branch_excel'); ?>'>
         <span class="glyphicon glyphicon-import">&nbsp</span><?php echo $this->lang->line('common_import_excel'); ?>
     </button>
 
@@ -68,7 +45,9 @@ $(document).ready(function()
         <button id="delete" class="btn btn-default btn-sm print_hide">
             <span class="glyphicon glyphicon-trash">&nbsp</span><?php echo $this->lang->line("common_delete"); ?>
         </button>
-        			
+		<button id="email" class="btn btn-default btn-sm">
+			<span class="glyphicon glyphicon-envelope">&nbsp</span><?php echo $this->lang->line("common_email");?>
+		</button>
     </div>
 </div>
 
