1e7fe72aaf5dbc9877c2e209d46c43c7e18c87c9
JhonatanPacheco <jhonatanpachecohernandez@gmail.com>
2018-05-10 09:12:25 -0500

    Add files via upload

---
+++ application/language/es/branch_office_lang.php
@@ -0,0 +1,7 @@
+<?php 
+
+$lang["branch_office_add"] = "Actualizar Artículo";
+$lang["branch_office_update"] = "Usar Menú de Inventario";
+$lang["branch_office_no_branch_office_to_display"] = "Sin datos que mostrar.";
+$lang["branch_office_new"]= "Nueva sucursal";
+$lang["branch_office_name"]= "Nombre Sucursal";
