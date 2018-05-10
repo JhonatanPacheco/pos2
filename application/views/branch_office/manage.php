c9527a063577eb2ce41aa23daf6a3daaed6612a5
JhonatanPacheco <jhonatanpachecohernandez@gmail.com>
2018-05-10 09:18:19 -0500

    Add files via upload

---
+++ application/views/branch_office/manage.php
@@ -0,0 +1,79 @@
+<?php $this->load->view("partial/header"); ?>
+
+<script type="text/javascript">
+$(document).ready(function()
+{
+    $('#generate_barcodes').click(function()
+    {
+        window.open(
+            'index.php/items/generate_barcodes/'+table_support.selected_ids().join(':'),
+            '_blank' // <- This is what makes it open in a new window.
+        );
+    });
+	
+	
+
+	// load the preset datarange picker
+	<?php $this->load->view('partial/daterangepicker'); ?>
+    // set the beginning of time as starting date
+//    $('#daterangepicker').data('daterangepicker').setStartDate("<?php echo date($this->config->item('dateformat'), mktime(0,0,0,01,01,2010));?>");
+	// update the hidden inputs with the selected dates before submitting the search data
+    var start_date = "<?php echo date('Y-m-d', mktime(0,0,0,01,01,2010));?>";
+	
+
+    $("#stock_location").change(function() {
+       table_support.refresh();
+    });
+
+    <?php $this->load->view('partial/bootstrap_tables_locale'); ?>
+
+    table_support.init({
+        employee_id: <?php echo $this->Employee->get_logged_in_employee_info()->person_id; ?>,
+        resource: '<?php echo site_url($controller_name);?>',
+        headers: <?php echo $table_headers; ?>,
+        pageSize: <?php echo $this->config->item('lines_per_page'); ?>,
+        uniqueId: 'branch_office.branch_office_id',
+        queryParams: function() {
+            return $.extend(arguments[0], {
+                start_date: start_date,
+                end_date: end_date,
+                stock_location: $("#stock_location").val(),
+                filters: $("#filters").val() || [""]
+            });
+        },
+        onLoadSuccess: function(response) {
+            $('a.rollover').imgPreview({
+				imgCSS: { width: 200 },
+				distanceFromCursor: { top:10, left:-210 }
+			})
+        }
+    });
+});
+</script>
+
+<div id="title_bar" class="btn-toolbar print_hide">
+    <button class='btn btn-info btn-sm pull-right modal-dlg' data-btn-submit='<?php echo $this->lang->line('common_submit') ?>' data-href='<?php echo site_url($controller_name."/excel_import"); ?>'
+            title='<?php echo $this->lang->line('items_import_items_excel'); ?>'>
+        <span class="glyphicon glyphicon-import">&nbsp</span><?php echo $this->lang->line('common_import_excel'); ?>
+    </button>
+
+    <button class='btn btn-info btn-sm pull-right modal-dlg' data-btn-new='<?php echo $this->lang->line('common_new') ?>' data-btn-submit='<?php echo $this->lang->line('common_submit') ?>' data-href='<?php echo site_url($controller_name."/view"); ?>'
+            title='<?php echo $this->lang->line($controller_name . '_new'); ?>'>
+        <span class="glyphicon glyphicon-tag">&nbsp</span><?php echo $this->lang->line($controller_name. '_new'); ?>
+    </button>
+</div>
+
+<div id="toolbar">
+    <div class="pull-left form-inline" role="toolbar">
+        <button id="delete" class="btn btn-default btn-sm print_hide">
+            <span class="glyphicon glyphicon-trash">&nbsp</span><?php echo $this->lang->line("common_delete"); ?>
+        </button>
+        			
+    </div>
+</div>
+
+<div id="table_holder">
+    <table id="table"></table>
+</div>
+
+<?php $this->load->view("partial/footer"); ?>
